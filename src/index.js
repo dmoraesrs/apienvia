feced5f7323c114e98ed7955746ab578ab5dfedc
Vinicius Gomes de Oliveira <vinicius.oliveira@intelbras.com.br>
2020-09-23 08:22:30 -0300

    Criando Rota Teste e add gitignore

--- src/index.js
+++ src/index.js
@@ -12,6 +12,12 @@ server.post('/enviar', (req, res) => {
   return res.json(cpf);
 });
 
+server.get('/teste', (req, res) => {
+  const msg = 'API ENVIA - OK';
+
+  return res.status(200).json({resposta: msg});
+});
+
 
 server.listen(3334, () => {
   console.log("API Envia Started!");
